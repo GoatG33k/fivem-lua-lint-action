ade7ea366f12be72b97b58febcf03939475aba38
GoatGeek <goatgeek@relta.net>
2021-06-02 21:25:15 -0700

    fix: improved error code handling

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -10,4 +10,15 @@ echo "exec => generate natives"
 node -r ts-node/register ./generate-rc.ts && cd $PWD
 
 echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH"
-cd $GITHUB_WORKSPACE && luacheck $LUACHECK_ARGS $LUACHECK_PATH
+cd $GITHUB_WORKSPACE 
+
+EXIT_CODE=0
+luacheck $LUACHECK_ARGS $LUACHECK_PATH || EXIT_CODE=$?
+echo "exit => $EXIT_CODE"
+if [ $EXIT_CODE -le 1 ]
+then
+ exit 0
+else
+ exit $EXIT_CODE
+fi
+ 
