d2f640f22abe2836d20a36d74f0c21829e517ae2
GoatGeek <goatgeek@relta.net>
2021-06-03 20:53:49 -0700

    feat: allow capturing output as file

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/sh -l
 LUACHECK_ARGS="--config /luacheck-fivem/.luacheckrc $1"
 LUACHECK_PATH="$2"
+LUACHECK_CAPTURE_OUTFILE="$GITHUB_WORKSPACE/$3"
 EXIT_CODE=0
+
 cd $GITHUB_WORKSPACE 
 
-echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH"
-luacheck $LUACHECK_ARGS $LUACHECK_PATH || EXIT_CODE=$?
+echo "outfile => $LUACHECK_CAPTURE_OUTFILE"
+
+if [[ ! -z "$LUACHECK_CAPTURE_OUTFILE" ]]; then
+  echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH 2>>$LUACHECK_CAPTURE_OUTFILE"
+  luacheck $LUACHECK_ARGS $LUACHECK_PATH >$LUACHECK_CAPTURE_OUTFILE 2>&1 || true
+  
+  echo "exec => luacheck $LUACHECK_ARGS --formatter default $LUACHECK_PATH"
+  luacheck $LUACHECK_ARGS --formatter default $LUACHECK_PATH || EXIT_CODE=$?
+else
+  echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH"
+  luacheck $LUACHECK_ARGS $LUACHECK_PATH || EXIT_CODE=$?
+fi
 
 echo "exit => $EXIT_CODE"
 if [ $EXIT_CODE -ge 2 ]; then
